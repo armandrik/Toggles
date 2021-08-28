3360e04f949a76ea5118e425ceb418005077b30e
mr.drik <83313133+armandrik@users.noreply.github.com>
2021-08-28 19:45:56 +0430

    Add files via upload

---
+++ Toggles/main.js
@@ -0,0 +1,185 @@
+const inputs = document.querySelectorAll("input");
+const body = document.querySelector('.body');
+var isColored = false;
+inputs[0].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #504f4f, #dbdbdb)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[1].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #2847e0, #e02828)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[2].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #ed1e79, #662d8c)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[3].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #02aabd, #00cdac)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[4].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #09203f, #537895)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[5].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #11998e, #38ef8d)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[6].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #654ea3, #eaafc8)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[7].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #ff0000, #ffcc00)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[8].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #334d50, #cbcaa5)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[9].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #799f0c, #acbb78)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[10].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #be5424, #eab091)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[11].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #5c4545, #5c5a45,#4a5c45,#455c5a,#48455c,#5c4559)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
+inputs[12].addEventListener("click", function() {
+    if (isColored) {
+        body.style.background = '#504f4f';
+        isColored = false;
+    } else {
+        body.style.background = 'linear-gradient(90deg, #a88080, #a8a580,#8ea880,#80a896,#8097a8,#8f80a8)';
+        body.style.backgroundSize = "400% 400%";
+        body.style.animationName = "gradient";
+        body.style.animationDuration = "10s";
+        body.style.animationTimingFunction = "ease";
+        body.style.animationIterationCount = "infinite";
+        isColored = true;
+    }
+})
